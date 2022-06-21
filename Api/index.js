b8a324d6a313e14785eb07a5bfd648ebb729b36c
Luzamir-medina <medinaluzamir@gmail.com>
2022-06-21 17:38:14 -0500

    actualizar comandos

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors  = require('cors')
 
-const app =express()
+const app = express()
 const port = 3000
 
 //#region
@@ -20,12 +20,12 @@ app.use(express.json({
 app.use(cors());
  //#region 
 // cuando te hagan un get http://localhost:3000/transactions
-app.get('/transactions',(req, res)=>{
+app.get('/transactions',(req, res)=> {
     res.send('me hicieron un get a app transactions')
 })
 // cuando te hagan un post  http://localhost:3000/transactions
 app.post('/transactions',(req, res)=>{
-    console.log("me esta llegando algo");
+    res.send('me hicieron un post')
     //res.send('Me hicieron un post')
 })
 
